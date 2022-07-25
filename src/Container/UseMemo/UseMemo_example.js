2ef15c3d8601c39b2732ded436802252c0fb72e6
nidhigolakiya06 <nidhigolakiya06@gmail.com>
2022-07-25 10:58:26 +0530

    usememo

---
+++ src/Container/UseMemo/UseMemo_example.js
@@ -0,0 +1,35 @@
+import React, { useMemo, useState } from 'react';
+
+function UseMemo_example(props) {
+
+    const [number, setNumber] = useState(0);
+    const [count , setCount] = useState(0);
+
+    const findfectorial = (n) => {
+        console.log("findfectorial");
+
+        if (n > 1) {
+            return (n* findfectorial(n-1))
+        }else {
+            return 1
+        }
+    }
+
+    // without memoized //
+    // const result = findfectorial(number)
+
+    // with memoized  //
+    const result = useMemo((e) =>  findfectorial(number) ,[number])
+
+    return (
+        <div>
+            <input type="text" placeholder='Place Enter Number' onChange={(e) => {setNumber(e.target.value)}}/>
+            <button onClick={() =>{setCount(count+1)}}>Counter</button>
+
+            <p>Enter ounter Number : {count}</p>
+            <p>Answer findfectorial : {result} </p>
+        </div>
+    );
+}
+
+export default UseMemo_example;
---
+++ src/Container/UsecallBack/UseCallBack.js
@@ -0,0 +1,32 @@
+import { height } from '@mui/system';
+import React, { useCallback, useState } from 'react';
+import ListItem from './ListItem';
+
+function UseCallBack(props) {
+    const [dark, setDark] = useState(false);
+    const [number, setNumber] = useState(0);
+
+    const theme = {
+        backgroundColor : dark ? 'black' : 'white' ,
+        color : dark ? 'white' : 'black',
+        height : "100vh"
+    }
+
+    const getItem = useCallback(
+        (i) => {
+            return [i+number, i+number+1, i+number+2]
+        },
+    [number] )
+
+
+    return (
+        <div style={theme}>
+            <button onClick={(e) => {setDark(!dark)}}>Change Theme</button>
+            <input type="number" placeholder="Please Enter Number" onChange={(e) => {setNumber(parseInt(e.target.value))}} />
+
+        <ListItem getItem={getItem} />
+        </div>
+    );
+}
+
+export default UseCallBack;
